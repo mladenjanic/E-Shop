14ec1cc6865a2a69c2a21c325f6f5a5b47c93ad1
themladjo <mladenjanic@gmail.com>
2019-12-15 21:39:55 +0100

    Adding views and message framevork

--- core/models.py
+++ core/models.py
@@ -1,20 +1,44 @@
 from django.conf import settings
 from django.db import models
+from django.shortcuts import reverse
+
+CATEGORY_CHOICES = (("S", "Shirt"), ("SW", "Sporwear"), ("OW", "Outwear"))
+
+LABEL_CHOICES = (("P", "primary"), ("S", "secondary"), ("D", "danger"))
 
 
 class Item(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    discount_price = models.DecimalField(
+        max_digits=20, decimal_places=2, blank=True, null=True
+    )
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={"slug": self.slug})
+
+    def get_add_to_cart(self):
+        return reverse("core:add-to-cart", kwargs={"slug": self.slug})
+
+    def get_remove_from_cart(self):
+        return reverse("core:remove-from-cart", kwargs={"slug": self.slug})
 
 
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=20, decimal_places=2)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.quantity} of {self.item.title}"
 
 
 class Order(models.Model):
